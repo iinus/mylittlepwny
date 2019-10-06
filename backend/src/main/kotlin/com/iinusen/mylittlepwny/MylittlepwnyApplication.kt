c45c20b18b6d14c1fd76122cfbdd1ae2ae63e4b1
Ina Bjornestad <iinusen7@gmail.com>
2019-10-06 18:20:48 +0200

    initial commit

---
+++ backend/src/main/kotlin/com/iinusen/mylittlepwny/MylittlepwnyApplication.kt
@@ -0,0 +1,11 @@
+package com.iinusen.mylittlepwny.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MylittlepwnyApplication
+
+fun main(args: Array<String>) {
+    runApplication<MylittlepwnyApplication>(*args)
+}
